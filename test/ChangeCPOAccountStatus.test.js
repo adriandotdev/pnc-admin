71fa7593231b12854fed9808710ef2fca6246bc4
Adrian Nads Marcelo <marcelonads@gmail.com>
2024-06-22 21:04:16 +0800

    development: IMPLEMENTED test cases for ChangeCPOAccountStatus function

--- test/ChangeCPOAccountStatus.test.js
+++ test/ChangeCPOAccountStatus.test.js
@@ -1,4 +1,8 @@
 const MerchantService = require("../services/MerchantService");
+const {
+	HttpInternalServerError,
+	HttpBadRequest,
+} = require("../utils/HttpError");
 
 jest.mock("mysql2", () => {
 	const mConnection = {
@@ -45,12 +49,80 @@ describe("Change CPO Account Status", () => {
 		expect(mockRepository.DeactivateCPOAccount).toHaveBeenCalledTimes(0);
 		expect(mockRepository.AuditTrail).toHaveBeenCalledTimes(1);
 		expect(mockRepository.AuditTrail).toHaveBeenCalledWith({
-			admin_id,
+			admin_id: 1,
 			cpo_id: null,
-			action: `${
-				action === "activate" ? "ACTIVATE" : "DEACTIVATE"
-			} Charging Point Operator account with id of ${userID}`,
+			action: `ACTIVATE Charging Point Operator account with id of ${1}`,
 			remarks: "success",
 		});
+	});
+
+	it("should successfully deactivate CPO account", async () => {
+		const result = await merchantService.ChangeCPOAccountStatus(
+			"deactivate",
+			1,
+			1
+		);
+
+		expect(result).toBe("SUCCESS");
+		expect(mockRepository.DeactivateCPOAccount).toHaveBeenCalledTimes(1);
+		expect(mockRepository.AuditTrail).toHaveBeenCalledWith({
+			admin_id: 1,
+			cpo_id: null,
+			action: `DEACTIVATE Charging Point Operator account with id of ${1}`,
+			remarks: "success",
+		});
+		expect(mockRepository.ActivateCPOAccount).toHaveBeenCalledTimes(0);
+		expect(mockRepository.AuditTrail).toHaveBeenCalledTimes(1);
+	});
+
+	it("should return NO_CHANGES_APPLIED", async () => {
+		mockRepository.ActivateCPOAccount = jest
+			.fn()
+			.mockResolvedValue({ affectedRows: 0 });
+
+		const result = await merchantService.ChangeCPOAccountStatus(
+			"activate",
+			1,
+			1
+		);
+
+		expect(result).toBe("NO_CHANGES_APPLIED");
+		expect(mockRepository.ActivateCPOAccount).toHaveBeenCalledTimes(1);
+		expect(mockRepository.DeactivateCPOAccount).toHaveBeenCalledTimes(0);
+		expect(mockRepository.AuditTrail).toHaveBeenCalledTimes(0);
+	});
+
+	it("should return an error when ACTIVATING CPO account fails", async () => {
+		mockRepository.ActivateCPOAccount = jest
+			.fn()
+			.mockRejectedValue(new HttpInternalServerError());
+
+		try {
+			await merchantService.ChangeCPOAccountStatus("activate", 1, 1);
+		} catch (err) {
+			expect(err).toBeInstanceOf(HttpInternalServerError);
+			expect(mockRepository.AuditTrail).toHaveBeenCalledTimes(1);
+			expect(mockRepository.AuditTrail).toHaveBeenCalledWith({
+				admin_id: 1,
+				cpo_id: null,
+				action: `ATTEMPT to DEACTIVATE Charging Point Operator account`,
+				remarks: "failed",
+			});
+		}
+	});
+
+	it("it should return BAD REQUEST when action is invalid", async () => {
+		try {
+			await merchantService.ChangeCPOAccountStatus("invalid", 1, 1);
+		} catch (err) {
+			expect(err).toBeInstanceOf(HttpBadRequest);
+			expect(mockRepository.AuditTrail).toHaveBeenCalledTimes(1);
+			expect(mockRepository.AuditTrail).toHaveBeenCalledWith({
+				admin_id: 1,
+				cpo_id: null,
+				action: `ATTEMPT to DEACTIVATE Charging Point Operator account`,
+				remarks: "failed",
+			});
+		}
 	});
 });
